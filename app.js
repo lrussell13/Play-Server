72134265c9ffacdd959925b53e0bb21c2a75e928
Lance Russell <lancebryanrussell@gmail.com>
2019-07-26 22:54:54 -0500

    first

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+const apps = require('./play-data.js');
+
+app.get('/apps', (req, res) => {
+    const { genre, sort} = req.query;
+
+    if(sort) {
+        if(!['Rating', 'App'].includes(sort)) {
+          return res
+            .status(400)
+            .send('Sort must be one of rating or app');
+        }
+      } 
+
+    if(genre) {
+        if(!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genre)) {
+          return res
+            .status(400)
+            .send('Sort must be one of Action, Puzzle, Strategy, Casual, Arcade, Card');
+        }
+      } 
+    
+    let results = apps;
+
+    if(genre){
+        results = apps
+        .filter(app=> 
+            app
+                .Genres
+                .toLowerCase()
+                .includes(genre.toLowerCase()));
+    } 
+
+    if(sort) {
+        results.sort((a, b) => {
+        return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+        }); 
+    } 
+
+    res
+        .json(results)
+});
+
+app.listen(8000, () => {
+  console.log('Server started on PORT 8000');
+});
